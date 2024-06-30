d2f28dafa7e583f44fc418372c267f5019f120dc
foysalBN <foysalbinnour@gmail.com>
2024-06-30 15:34:40 +0600

    test js clearnup

---
+++ tests/res.js
@@ -0,0 +1,18 @@
+let a = {
+
+  26432: {
+    "74026 - Cox's Bazar Model Polytechnic Institute": [689269, 689277],
+    "74040 - Saikat Polytechnic Institute": [689346]
+  },
+  26444: {
+    "74026 - Cox's Bazar Model Polytechnic Institute": [689269, 689277, 689281],
+    "74040 - Saikat Polytechnic Institute": [689330]
+  },
+  25841: { ...},
+  26441: { ...},
+  26443: { ...},
+  26711: { ...},
+  26811: { ...},
+  26831: { ...},
+  28533: { ...},
+}
